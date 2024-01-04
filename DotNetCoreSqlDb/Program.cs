4e48f05c2074ccce0b0faab2d482d30a8602853b
BasRoz <155643861+BasRoz@users.noreply.github.com>
2024-01-04 17:35:07 +0100

    Connection string

--- DotNetCoreSqlDb/Program.cs
+++ DotNetCoreSqlDb/Program.cs
@@ -5,11 +5,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add database context and cache
 builder.Services.AddDbContext<MyDatabaseContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString(AZURE_SQL_CONNECTIONSTRING)));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
 //builder.Services.AddDistributedMemoryCache();
 builder.Services.AddStackExchangeRedisCache(options =>
 {
-options.Configuration = builder.Configuration[AZURE_REDIS_CONNECTIONSTRING];
+options.Configuration = builder.Configuration["AZURE_REDIS_CONNECTIONSTRING"];
 options.InstanceName = "SampleInstance";
 });
 
